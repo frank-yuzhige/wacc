d309cc72e09f64aad94ac00a1f8c014b9aef12ba
Ruoyu Hu <rh4618@texel04.doc.ic.ac.uk>
2020-02-10 12:24:57 +0000

    Removed erroneous import of Register in InstructionNode

--- src/main/kotlin/codegen/InstructionNode.kt
+++ src/main/kotlin/codegen/InstructionNode.kt
@@ -1,7 +1,6 @@
 package codegen
 
 import codegen.arm.Instruction
-import codegen.arm.Register
 
 class InstructionBlock(val label: String, val instructions: List<Instruction>, val successors: List<InstructionBlock>) {
     fun getTerminator(): Instruction = instructions.last()
