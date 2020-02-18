94913b2c2ff476c7fefc7d2b9a5aea4b1b211250
zy7218 <zhige.yu18@imperial.ac.uk>
2020-02-18 11:38:08 +0000

    Add cmdline intepreter file output

--- src/main/kotlin/CmdLineInterpreter.kt
+++ src/main/kotlin/CmdLineInterpreter.kt
@@ -3,24 +3,37 @@ import exceptions.SemanticException
 import exceptions.SyntacticException
 import parser.Parser
 import semantics.SemanticAnalyzer
+import sun.swing.FilePane
 import utils.ERROR
 import utils.RESET
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileNotFoundException
+import java.nio.file.Path
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
-    val inputStream = if (args.isNotEmpty()) {
-        try {
-            FileInputStream(args[0])
-        } catch (fnfe: FileNotFoundException) {
-            println("File not found!")
-            println("Unable to locate file at ${args[0]}")
-            exitProcess(1)
+    var debug = false
+
+    val flags = args.filter { it.startsWith("-") }
+    flags.forEach{ flag ->
+        when (flag) {
+            "-d" -> {
+                println("** SYSTEM: DEBUG MODE ACTIVATED **")
+                debug = true
+            }
+            else -> throw UnsupportedOperationException("Unknown flag \"$flag\"!")
         }
-    } else {
-        System.`in`
+    }
+
+    val filePath = args[flags.size]
+            ?: throw FileNotFoundException("Empty file path!")
+    val inputStream = try {
+        FileInputStream(filePath)
+    } catch (fnfe: FileNotFoundException) {
+        println("File not found!")
+        println("Unable to locate file at ${args[0]}")
+        exitProcess(1)
     }
 
     val sa = SemanticAnalyzer()
@@ -36,11 +49,10 @@ fun main(args: Array<String>) {
         System.err.println("${se.msg}$RESET")
         exitProcess(200)
     }
-    val debug = true
     val asmPath = if (debug) {
         "src/test/resources/valid/mine/gen.s"
     } else {
-        args[0].substringBeforeLast('.', "") + ".s"
+        File(filePath).nameWithoutExtension + ".s"
     }
 
     println("===========")
